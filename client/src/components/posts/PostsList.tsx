fb1dc8de61c36c99bb6b477c3a1be566975d1e9a
Santola <gabrielsantolin52@gmail.com>
2024-07-27 17:06:35 -0300

    Fixed missing Edit Links
    Signed-off-by: Santola <gabrielsantolin52@gmail.com>

--- client/src/components/posts/PostsList.tsx
+++ client/src/components/posts/PostsList.tsx
@@ -54,6 +54,8 @@ function PostsList() {
             </Link>
           </h2>
           <div className='post-links'>
+            <Link to={`/posts/${post.id}/edit`}>Edit</Link>
+            {' | '}
             <button onClick={() => deletePost(post.id)}>Delete</button>
           </div>
         </div>
